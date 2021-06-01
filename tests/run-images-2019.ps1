06c4a530bcc74cd71e6fc6d3abdc9f3ff4009051
Ivan Kozlovic <ivan@synadia.com>
2021-06-01 15:48:40 -0600

    Release v0.22.0
    Signed-off-by: Ivan Kozlovic <ivan@synadia.com>

--- tests/run-images-2019.ps1
+++ tests/run-images-2019.ps1
@@ -4,8 +4,8 @@ Set-PSDebug -Trace 2
 $ErrorActionPreference = "Stop"
 
 $images = @(
-	"nats-streaming:0.21.2-windowsservercore-1809",
-	"nats-streaming:0.21.2-nanoserver-1809"
+	"nats-streaming:0.22.0-windowsservercore-1809",
+	"nats-streaming:0.22.0-nanoserver-1809"
 )
 
 foreach ($img in $images) {
