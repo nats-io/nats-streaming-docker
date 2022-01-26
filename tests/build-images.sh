eb75414c797fa4d4ae8cd2f295c7d36cb124dc16
Ivan Kozlovic <ivan@synadia.com>
2022-01-26 14:22:18 -0700

    Release v0.24.0
    - Removed lts2016 since it is EOL
    - Updated alpine to 3.15
    
    Signed-off-by: Ivan Kozlovic <ivan@synadia.com>

--- tests/build-images.sh
+++ tests/build-images.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -ex
 
-ver=(NATS_STREAMING_SERVER 0.23.2)
+ver=(NATS_STREAMING_SERVER 0.24.0)
 
 (
-	cd "${ver[1]}/alpine3.14"
-	docker build --tag nats-streaming:0.23.2-alpine3.14 .
+	cd "${ver[1]}/alpine3.15"
+	docker build --tag nats-streaming:0.24.0-alpine3.15 .
 )
 
 (
 	cd "${ver[1]}/scratch"
-	docker build --tag nats-streaming:0.23.2-scratch .
+	docker build --tag nats-streaming:0.24.0-scratch .
 )
