4e3f45623cc8939b46d59ffb8e9373b012a52f76
Ivan Kozlovic <ivan@synadia.com>
2022-02-04 18:18:06 -0700

    Release v0.24.1
    Signed-off-by: Ivan Kozlovic <ivan@synadia.com>

--- tests/build-images.sh
+++ tests/build-images.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -ex
 
-ver=(NATS_STREAMING_SERVER 0.24.0)
+ver=(NATS_STREAMING_SERVER 0.24.1)
 
 (
 	cd "${ver[1]}/alpine3.15"
-	docker build --tag nats-streaming:0.24.0-alpine3.15 .
+	docker build --tag nats-streaming:0.24.1-alpine3.15 .
 )
 
 (
 	cd "${ver[1]}/scratch"
-	docker build --tag nats-streaming:0.24.0-scratch .
+	docker build --tag nats-streaming:0.24.1-scratch .
 )
