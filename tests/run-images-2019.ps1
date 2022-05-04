47224b3c05dc5a5620a0bd9d90f51822054923a9
Ivan Kozlovic <ivan@synadia.com>
2022-05-04 16:55:49 -0600

    Release v0.24.6
    Signed-off-by: Ivan Kozlovic <ivan@synadia.com>

--- tests/run-images-2019.ps1
+++ tests/run-images-2019.ps1
@@ -4,8 +4,8 @@ Set-PSDebug -Trace 2
 $ErrorActionPreference = "Stop"
 
 $images = @(
-	"nats-streaming:0.24.5-windowsservercore-1809",
-	"nats-streaming:0.24.5-nanoserver-1809"
+	"nats-streaming:0.24.6-windowsservercore-1809",
+	"nats-streaming:0.24.6-nanoserver-1809"
 )
 
 foreach ($img in $images) {
--- tests/run-images.sh
+++ tests/run-images.sh
@@ -2,8 +2,8 @@
 set -ex
 
 images=(
-	'nats-streaming:0.24.5-alpine3.15'
-	'nats-streaming:0.24.5-scratch'
+	'nats-streaming:0.24.6-alpine3.15'
+	'nats-streaming:0.24.6-scratch'
 )
 
 for img in "${images[@]}"; do
